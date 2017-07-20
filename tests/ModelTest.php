1839cf68339f21510c87550639cfafc2b75ec0c5
Junior Grossi <juniorgro@gmail.com>
2017-07-20 13:39:07 -0300

    Add test case to make sure the object attributes are not send as array

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 class ModelTest extends \PHPUnit\Framework\TestCase
 {
     /**
@@ -15,7 +17,21 @@ class ModelTest extends \PHPUnit\Framework\TestCase
         $this->assertNotEmpty($attributes);
         $this->assertArrayHasKey('first_name', $attributes);
         $this->assertEquals('John', $attributes['first_name']);
-    }    
+    }
+
+    /**
+     * @test
+     */
+    public function it_sends_attributes_correctly()
+    {
+        $user = new User(['logged_at' => Carbon::create(2017, 07, 01)]);
+
+        $attributes = $user->toArray();
+
+        $this->assertNotEmpty($attributes);
+        $this->assertArrayHasKey('logged_at', $attributes);
+        $this->assertEquals('2017', $attributes['logged_at']);
+    }
 }
 
 class User extends \Illuminate\Database\Eloquent\Model
@@ -31,5 +47,10 @@ class UserMutator extends \Jgrossi\Mutable\Mutator
     public function firstName($value)
     {
         return ucfirst($value);
+    }
+
+    public function loggedAt(Carbon $date)
+    {
+        return $date->format('Y');
     }
 }
